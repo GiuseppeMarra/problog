a351b4118fa699f73f2cd99a5b133fba5b9540c5
paolomorettin <morettin.paolo@gmail.com>
2020-11-16 17:54:03 +0100

    updated example

--- hackathon_tests/mln1.py
+++ hackathon_tests/mln1.py
@@ -3,25 +3,29 @@ from problog.logic import Term
 from problog.engine import DefaultEngine
 from problog.sdd_formula import SDD
 
+from sys import argv
 
+pc = 0.2 # c = a v b
+# P(A) = P(A | c)P(c) + P(A | not c)(1-P(c))
+# 2/3 * 2/10 + 1/2 * 8/10 = 32/60
 
-#p = PrologFile('path/to/myprogram.pl')
-p = PrologString("""
+p = PrologString(f"""
 
 0.5:: a.
 0.5:: b.    
 
 d:- a, \+b.
 
-p = ew / (ew +1)
+%p = ew / (ew +1)
 
-0.2::c.
+{1-pc}::c_aux.
+c :- c_aux.
 
 c:- a.
 c:- b.
 constraint(c).
-query(d).
-query(c).
+%query(d).
+%query(c).
 query(a).
 
 """)
@@ -41,3 +45,7 @@ sdd = SDD.createFrom(gp) # Use SDDs
 result = sdd.evaluate()
 
 print(result)
+
+print("mln", 32/60)
+print((2 * 0.2 * 0.25 + 2*0.8*0.25)/(4 * 0.8 * 0.25 + 3*0.2*0.25))
+print((2 * 0.2 * 0.25 + 2*0.8*0.25)/(4 * 0.8 * 0.25 + 4*0.2*0.25))
